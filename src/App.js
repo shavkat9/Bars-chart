1941c6c0d4103666157c95f0c59dcfd4361d0146
Shavkat Berdibekov <103271898+shavkat9@users.noreply.github.com>
2023-01-04 21:02:51 +0500

    Bars

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Bars from './Components/Bars';
+
+
+
+const  App = () =>  {
+  return (
+    <div className="container">
+      < Bars />
+    </div>
+
+  );
+};
+
+export default App;
+ 
---
+++ src/Components/Bars.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BarsLine from './BarsLine';
+import './Style.css';
+
+function Bars() {
+  return (
+    <>
+    < BarsLine />
+    </>
+  )
+}
+
+export default Bars
